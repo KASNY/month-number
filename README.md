5062baa861730cac07195c10727737f414cb9dc0
KASNY <75784852+KASNY@users.noreply.github.com>
2023-11-26 13:28:20 +0100

    v1.0
    Added program

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # month-number
 Counting the day of the month and leap year
+
+Start the project, enter the month number 1-12 (if you enter a letter or a number outside the given range, the program will write that you entered an incorrect number), and then enter the year you are interested in.
---
+++ month-number/month-number.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	int month_number;
+	std::cout << "Enter the month number: ";
+	if (!(std::cin >> month_number))
+	{
+		std::cerr << "Its not number!";
+		std::exit(0);
+	}
+
+	switch (month_number)
+	{
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
+		std::cout << "This month have 31 days!";
+		break;
+
+	case 4:
+	case 6:
+	case 9:
+	case 11:
+		std::cout << "This month have 30 days!";
+		break;
+
+	case 2:
+	{
+		int year;
+		std::cout << "Enter the year: ";
+		std::cin >> year;
+
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+		{
+			std::cout << "This month have 29 days!";
+		}
+		else
+		{
+			std::cout << "This month have 28 days!";
+		}
+	}
+
+	break;
+
+	default:
+		std::cout << "Worst number!";
+
+	}
+}
+
